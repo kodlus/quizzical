ae76a529f2d5e7702cbe0f3c440f1af7a214e226
kodlus <nihoff@tuta.io>
2024-05-15 12:37:35 +0200

    Added a scroll to top feature when OpeningScreen mounts

--- src/components/OpeningScreen.jsx
+++ src/components/OpeningScreen.jsx
@@ -3,12 +3,19 @@
 /*=================================================
 Import
 =================================================*/
+import { useEffect } from "react";
 import OptionsForm from "./OptionsForm"
 
 /*=================================================
 OpeningScreen
 =================================================*/
 const OpeningScreen = (props) => {
+
+  // Scrolls to the top when OpeningScreen mounts
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <section className="opening-screen">
       <h1>Quizzical</h1>
